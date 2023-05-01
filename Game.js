02e9fe0997af68373a3ae63dd5621bdc12d6156a
Danny Van Stemp <danwardvs@adsgames.net>
2023-05-01 13:57:47 -0400

    code cleanup

--- Game.js
+++ Game.js
@@ -1,11 +1,6 @@
 let coin;
 let money=0;
 
-// Setup enum for types
-const COINS = {
-  PER_CLICK: 0,
-  PER_SECOND: 1
-}
 var myGame;
 let old_mouse_z = 0;
 let second_timer = 0;
@@ -186,6 +181,7 @@ function update()
       
   }
 	
+  // Cheats
 	if(key[KEY_I])
 		money+=10000;
 	
@@ -213,35 +209,17 @@ function update()
 
 }
 
-function setup(){
+ function setup(){
 
     myGame = new Game();
-	//Creates an item based on the item class
-    //The arguments are x position, y position, money type, initial cost, amount of money received, and name.
     const item_space = 40;
 
-    game_items.push( new Item(550,menu_origin+0*item_space,COINS.PER_SECOND,100,2,"Inmate","inmate"));
-    game_items.push( new Item(550,menu_origin+1*item_space,COINS.PER_CLICK,100,2,"JedCoin Press","press"));
-    game_items.push( new Item(550,menu_origin+2*item_space,COINS.PER_SECOND,750,10,"Workstation","workstation"));
-    game_items.push( new Item(550,menu_origin+3*item_space,COINS.PER_SECOND,2000,25,"JedCoin Mine","mine"));
-    game_items.push( new Item(550,menu_origin+4*item_space,COINS.PER_SECOND,20000,100,"JedCoin Forge","forge"));
-    game_items.push( new Item(550,menu_origin+5*item_space,COINS.PER_SECOND,35000,300,"Jedetically Modified Crop","jmocrop"));
-    game_items.push( new Item(550,menu_origin+6*item_space,COINS.PER_CLICK,40000,100,"Jed Clone","jedclone"));
-    game_items.push( new Item(550,menu_origin+7*item_space,COINS.PER_SECOND,50000,750,"Nuclear Power Plant","powerplant"));
-    game_items.push( new Item(550,menu_origin+8*item_space,COINS.PER_SECOND,100000,2000,"Village","village"));
-    game_items.push( new Item(550,menu_origin+9*item_space,COINS.PER_SECOND,1250000,15000,"Space Station","spacestation"));
-    game_items.push( new Item(550,menu_origin+10*item_space,COINS.PER_SECOND,2000000,50000,"Moon","moon"));
-    game_items.push( new Item(550,menu_origin+11*item_space,COINS.PER_CLICK,7500000,5000,"JeDOS AI","jedos"));
-    game_items.push( new Item(550,menu_origin+12*item_space,COINS.PER_SECOND,100000000,500000,"Space Teleporter","teleporter"));
-    game_items.push( new Item(550,menu_origin+13*item_space,COINS.PER_CLICK,150000000,25000,"Electromagnetic Coin Magnifier","magnet"));
-    game_items.push( new Item(550,menu_origin+14*item_space,COINS.PER_SECOND,500000000,1000000,"Killer Robot","robot"));
-    game_items.push( new Item(550,menu_origin+15*item_space,COINS.PER_SECOND,1000000000,3000000,"Dark Matter Coin","darkcoin"));
-    game_items.push( new Item(550,menu_origin+16*item_space,COINS.PER_SECOND,2500000000,6000000,"Starship Fleet","starship"));
-    game_items.push( new Item(550,menu_origin+17*item_space,COINS.PER_SECOND,7500000000,20000000,"Distant Inhabitable Planet","planet"));
-    game_items.push( new Item(550,menu_origin+18*item_space,COINS.PER_SECOND,10000000000,80000000,"Black Hole","blackhole"));
-    game_items.push( new Item(550,menu_origin+19*item_space,COINS.PER_SECOND,150000000000,200000000,"Jed Salt","jedsalt"));
-    game_items.push( new Item(550,menu_origin+20*item_space,COINS.PER_SECOND,100000000000000,30000000000,"Jed","jed"));
-    game_items.push( new Item(550,menu_origin+21*item_space,COINS.PER_SECOND,7,0,"Depressed Cookie","cookie"));
+    game_data.forEach((item,i)=>{
+      const type = item.type===""
+      game_items.push( new Item(550,menu_origin+i*item_space,item.type,item.cost,item.value,item.name,item.image));
+
+    })
+
     
 	coin = load_bmp("images/coin.png");
 	
---
+++ constants.js
@@ -0,0 +1,5 @@
+// Setup enum for types
+const COINS = {
+  PER_CLICK: 0,
+  PER_SECOND: 1,
+};
